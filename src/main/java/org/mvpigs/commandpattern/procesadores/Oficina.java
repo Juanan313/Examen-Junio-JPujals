6f7002e61ed12483d98361b962ddd955680fd6b0
Unknown <juanan.pujals@gmail.com>
2018-06-07 17:32:42 +0200

    test_printar_status Pasa Caso Test

--- src/main/java/org/mvpigs/commandpattern/procesadores/Oficina.java
+++ src/main/java/org/mvpigs/commandpattern/procesadores/Oficina.java
@@ -1,5 +1,6 @@
 package org.mvpigs.commandpattern.procesadores;
 
+import org.mvpigs.commandpattern.interfaces.Pedido;
 import org.mvpigs.commandpattern.interfaces.Procesador;
 import org.mvpigs.commandpattern.interfaces.TratamientoPedido;
 
@@ -8,5 +9,18 @@ public class Oficina implements Procesador {
 
     public boolean procesa(TratamientoPedido pedido) {
         return pedido.tratar();
+    }
+
+    public enum EstadoPedido {
+        ACEPTADO, RECHAZADO
+    }
+
+    public String printarStatus(Boolean procesa, Pedido pedidoAProcesar) {
+        
+        if ( procesa == true ) {
+            return pedidoAProcesar.destino()+" " + EstadoPedido.ACEPTADO.name();
+        }
+
+        return pedidoAProcesar.destino()+" " + EstadoPedido.RECHAZADO.name();
     }
 }

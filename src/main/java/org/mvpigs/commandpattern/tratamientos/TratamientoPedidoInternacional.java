6bdc41d435d619d0079d63f491698cb3ba310235
Unknown <juanan.pujals@gmail.com>
2018-06-07 16:44:28 +0200

    primer caso Test pasa

--- src/main/java/org/mvpigs/commandpattern/tratamientos/TratamientoPedidoInternacional.java
+++ src/main/java/org/mvpigs/commandpattern/tratamientos/TratamientoPedidoInternacional.java
@@ -1,8 +1,22 @@
 package org.mvpigs.commandpattern.tratamientos;
 
+import org.mvpigs.commandpattern.interfaces.Pedido;
 import org.mvpigs.commandpattern.interfaces.TratamientoPedido;
 
 public class TratamientoPedidoInternacional implements TratamientoPedido {
 
-    
+    public Pedido pedidoATratar;
+
+    public TratamientoPedidoInternacional( Pedido pedidoInternacional) {
+        this.pedidoATratar = pedidoInternacional;
+    }
+
+    public boolean tratar() {
+
+        if (pedidoATratar.destino() == "Mordor") {
+            return false;
+        } 
+        
+        return true;
+    }
 }
